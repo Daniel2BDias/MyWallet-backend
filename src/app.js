8fd264887c773174a7268531f96427bb6e8ac821
Daniel Dias <daniiel2dias@gmail.com>
2023-07-05 13:15:59 -0300

    Rota: POST nova-transacao

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import { MongoClient } from "mongodb";
 import joi from "joi";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
+import dayjs from "dayjs";
+
 
 const app = express();
 
@@ -57,6 +59,11 @@ app.post("/cadastro", async (req, res) => {
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
+  const { error } = loginSchema.validate(req.body, { abortEarly: false });
+
+  if (error)
+    return res.status(422).send(error.details.map((detail) => detail.message));
+
   try {
     const existingUser = await db.collection("users").findOne({ email });
     if (!existingUser) return res.sendStatus(404);
@@ -64,19 +71,56 @@ app.post("/login", async (req, res) => {
     if (!pWordValid) return res.sendStatus(401);
 
     const token = uuid();
-    
+
     const inSession = await db.collection("sessions").findOne({ email });
-    if(inSession) return res.status(409).send("Already in Session! Please, logout");
+    if (inSession)
+      return res.status(409).send("Already in Session! Please, logout");
 
     await db.collection("sessions").insertOne({ token, email });
 
-    res.status(201).send({token, email, name: existingUser.name});
+    res.status(201).send({ token, email, name: existingUser.name });
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
 
-app.post("/", () => {});
+app.post("/nova-transacao/:tipo", async (req, res) => {
+  const { authorization } = req.headers;
+  const { tipo} = req.params;
+  const { value, description } = req.body;
+
+  if (tipo !== "add" && tipo !== "subtract") return res.sendStatus(404);
+
+  const token = authorization?.replace("Bearer ", "");
+
+  if (!token) return res.sendStatus(401);
+
+  try {
+    const user = await db.collection("sessions").findOne({ token });
+
+    if (!user) return res.send(401);
+
+    await db
+      .collection("transaction")
+      .insertOne({
+        id: user._id,
+        email: user.email,
+        transaction: { value, description, type: tipo, time: `${dayjs().format("DD/MM")}` },
+      });
+
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.get("/transactions", async (req, res) => {
+    try {
+
+    } catch (error) {
+      
+    }
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor Online! PORT: ${process.env.PORT}`);
