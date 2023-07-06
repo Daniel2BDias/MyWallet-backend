5bde7b47f61ead3a920ef2f5128574cec0ef8e7e
Daniel Dias <daniiel2dias@gmail.com>
2023-07-06 09:33:08 -0300

    schema altered

--- src/schemas/transactionsSchemas.js
+++ src/schemas/transactionsSchemas.js
@@ -2,5 +2,5 @@ import joi from "joi";
 
 export const transactionSchema = joi.object().keys({
     value: joi.number().required(),
-    description: joi.string().max(10).required(),
+    description: joi.string().required(),
   });
